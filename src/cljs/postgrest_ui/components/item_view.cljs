bd8fb2a06cf77ad3edc74bba7b64a552d8093b3e
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-07-31 13:44:55 +0300

    Allow specifying a view component

--- src/cljs/postgrest_ui/components/item_view.cljs
+++ src/cljs/postgrest_ui/components/item_view.cljs
@@ -6,23 +6,29 @@
             [postgrest-ui.elements :refer [element]])
   (:require-macros [postgrest-ui.impl.state :refer [define-stateful-component]]))
 
-(define-stateful-component item-view [{:keys [endpoint table select style]} primary-key]
+
+(define-stateful-component item-view [{:keys [endpoint table select style view]} primary-key]
   {:state state}
   (if-let [defs @(registry/load-defs endpoint)]
     (if-let [{:keys [loaded-item]} @state]
-      (element style :item-view
-               (doall
-                (for [column select
-                      :let [value (get loaded-item (if (map? column)
-                                                     (:table column)
-                                                     column))]]
-                  (with-meta
-                    (element style :item-view-field
-                             (element style :item-view-label
-                                      (display/label table column))
-                             (element style :item-view-value
-                                      [display/disp :item-view table column value]))
-                    {:key column}))))
+      (if view
+        ;; If a view component is specified, use it
+        [view loaded-item]
+
+        ;; Otherwise use generic item view
+        (element style :item-view
+                 (doall
+                  (for [column select
+                        :let [value (get loaded-item (if (map? column)
+                                                       (:table column)
+                                                       column))]]
+                    (with-meta
+                      (element style :item-view-field
+                               (element style :item-view-label
+                                        (display/label table column))
+                               (element style :item-view-value
+                                        [display/disp :item-view table column value]))
+                      {:key column})))))
 
       ;; Load item
       (do
