22f887568b189ce8e876e164a7d9feb838fc855d
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-08-09 13:31:08 +0300

    Simple component to show query results

---
+++ src/cljs/postgrest_ui/components/query.cljs
@@ -0,0 +1,23 @@
+(ns postgrest-ui.components.query
+  "Query component for showing data based on a query."
+  (:require [postgrest-ui.impl.fetch :as fetch]
+            [postgrest-ui.impl.registry :as registry]
+            [postgrest-ui.elements :refer [element]])
+  (:require-macros [postgrest-ui.impl.state :refer [define-stateful-component]]))
+
+
+(define-stateful-component query [{:keys [endpoint token table select order-by where style]} view-fn]
+  {:state state
+   :component-will-receive-props
+   (reset! state {})}
+  (if-let [defs @(registry/load-defs endpoint)]
+    (let [results (:results state)]
+      (when (nil? results)
+        (.then (fetch/load-range endpoint token defs
+                                 (select-keys opts [:table :select :order-by :where])
+                                 nil nil)
+               #(assoc state :results %)))
+      (if results
+        [view-fn results]
+        (element style :loading-indicator)))
+    (element style :loading-indicator)))
